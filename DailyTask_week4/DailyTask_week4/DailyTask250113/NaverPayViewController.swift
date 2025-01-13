f730cbf347951b14fe044a4c24fba33ed096eb37
ParkSY0919 <qhr498@gmail.com>
2025-01-13 17:55:28 +0900

    [Add/#1] add NaverPayViewController

---
+++ DailyTask_week4/DailyTask_week4/DailyTask250113/NaverPayViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NaverPayViewController.swift
+//  DailyTask_week4
+//
+//  Created by 박신영 on 1/13/25.
+//
+
+import UIKit
+
+class NaverPayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func setHierarchy() {}
+    func setLayout() {}
+    func setStyle() {}
+
+}
