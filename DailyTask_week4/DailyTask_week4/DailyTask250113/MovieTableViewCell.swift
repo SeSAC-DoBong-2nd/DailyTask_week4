a7465a53ae589d5241018bde28abfc3603a1babb
ParkSY0919 <qhr498@gmail.com>
2025-01-13 19:14:29 +0900

    [Add/#1] add MovieTableViewCell

---
+++ DailyTask_week4/DailyTask_week4/DailyTask250113/MovieTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieTableViewCell.swift
+//  DailyTask_week4
+//
+//  Created by 박신영 on 1/13/25.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    static let identifier = "MovieTableViewCell"
+
+}
