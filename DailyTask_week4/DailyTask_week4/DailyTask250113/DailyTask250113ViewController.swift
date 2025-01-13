a8d3e23406d43db7dbd3ab70f38a3b7a39f68582
ParkSY0919 <qhr498@gmail.com>
2025-01-13 17:49:57 +0900

    [Feat/#1] 코드로 오토레이아웃 및 UI 구성해보기 - storyboard 조건 구현

---
+++ DailyTask_week4/DailyTask_week4/DailyTask250113/DailyTask250113ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DailyTask250113ViewController.swift
+//  DailyTask_week4
+//
+//  Created by 박신영 on 1/13/25.
+//
+
+import UIKit
+
+class DailyTask250113ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = ._250109Background
+    }
+
+    @IBAction
+    func netflixBtnTapped(_ sender: UIButton) {
+        print(#function)
+        let vc = NetflixViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @IBAction
+    func naverPayBtnTapped(_ sender: UIButton) {
+        print(#function)
+    }
+    
+    @IBAction
+    func movieListBtnTapped(_ sender: UIButton) {
+        print(#function)
+    }
+    
+    
+}
