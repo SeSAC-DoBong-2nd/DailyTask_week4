b9ead979391bdedae6315c2311e1c55081b71c87
ParkSY0919 <qhr498@gmail.com>
2025-01-15 00:28:33 +0900

    [Add/#3] add LotteryViewController

---
+++ DailyTask_week4/DailyTask_week4/DailyTask250114/LotteryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LotteryViewController.swift
+//  DailyTask_week4
+//
+//  Created by 박신영 on 1/15/25.
+//
+
+import UIKit
+
+class LotteryViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    override func setHierarchy() {
+        
+    }
+    
+    override func setLayout() {
+        
+    }
+    
+    override func setStyle() {
+        
+    }
+
+}
