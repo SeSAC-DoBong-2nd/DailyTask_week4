559d046adfc203475789affb407066d3a182a76b
ParkSY0919 <qhr498@gmail.com>
2025-01-15 15:52:59 +0900

    [Feat/#5] 메인 화면 - UI 구현

--- DailyTask_week4/DailyTask_week4/DailyTask250115/NaverSearchViewController.swift
+++ DailyTask_week4/DailyTask_week4/DailyTask250115/NaverSearchViewController.swift
@@ -7,23 +7,71 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class NaverSearchViewController: BaseViewController {
+    
+    let searchBar = UISearchBar()
+    let emptyView = UIView()
+    let imageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setRegister()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func setHierarchy() {
+        view.addSubviews(searchBar, imageView)
+        
+//        emptyView.addSubview()
     }
-    */
+    
+    override func setLayout() {
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(40)
+        }
+        
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom).offset(170)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(320)
+            $0.height.equalTo(300)
+        }
+    }
+    
+    override func setStyle() {
+        view.backgroundColor = .black
+        
+        navigationItem.title = "psy의 쇼핑쇼핑"
+        
+        searchBar.do {
+            $0.placeholder = "브랜드, 상품, 프로필, 태그 등"
+        }
+        
+        imageView.do {
+            $0.image = UIImage(resource: .emptyView)
+        }
+    }
 
+}
+
+private extension NaverSearchViewController {
+    
+    func setRegister() {
+        searchBar.delegate = self
+    }
+    
+}
+
+extension NaverSearchViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print(#function, searchBar.text ?? "")
+        view.endEditing(true)
+    }
+    
 }
