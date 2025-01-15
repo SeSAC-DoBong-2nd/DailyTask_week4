783833d76927ce3895fdfb46761e56be6662876c
ParkSY0919 <qhr498@gmail.com>
2025-01-15 14:45:20 +0900

    [Add/#5] add DailyTask250115 Related files

---
+++ DailyTask_week4/DailyTask_week4/DailyTask250115/NaverSearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NaverSearchViewController.swift
+//  DailyTask_week4
+//
+//  Created by 박신영 on 1/15/25.
+//
+
+import UIKit
+
+class NaverSearchViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
